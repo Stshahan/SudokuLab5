c34877971c071099fac8d06ddd8fe76c40851312
ryanw <ryanw@128.4.103.49>
2019-04-24 19:32:34 -0400

    more work on day

--- SudokuBLL/src/main/java/pkgEnum/Day.java
+++ SudokuBLL/src/main/java/pkgEnum/Day.java
@@ -1,10 +1,22 @@
 package pkgEnum;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 public enum Day implements Serializable, Comparable<Day>{
 
 	 MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY,SUNDAY;
 	
 	private String abbreviation;
+	
+	private static Map<String,Day> lookup = new HashMap<String,Day>();
+
+	public String getAbbreviation() {
+		return abbreviation;
+	}
+	
+	public static Day get (String abbreviation) {
+		return lookup.get(abbreviation);
+	}
 }
